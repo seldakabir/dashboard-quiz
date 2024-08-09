dc77392af708a18aa4874985b5c33d1dc461d698
selda kabir <seldaakabirnejat@gmail.com>
2024-08-10 00:02:24 +0300

    createPages

---
+++ src/Components/Header.js
@@ -0,0 +1,13 @@
+import React from "react";
+
+export default function Header() {
+  return (
+    <div className="header">
+      <img src="./Images/logo.png" alt="logo" className="logo"></img>
+      <div className="sign">
+        <p>Sing Up</p>
+        <p>Sing In</p>
+      </div>
+    </div>
+  );
+}
---
+++ src/Pages/EditQuiz.js
@@ -0,0 +1,5 @@
+import React from "react";
+
+export default function EditQuiz() {
+  return <div>EditQuiz</div>;
+}
