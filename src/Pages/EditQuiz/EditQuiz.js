7f811764006dc15d2c78fa589b833bef6eaea2b4
selda kabir <seldaakabirnejat@gmail.com>
2024-08-27 00:45:08 +0300

    AddCss

--- src/Pages/EditQuiz/EditQuiz.js
+++ src/Pages/EditQuiz/EditQuiz.js
@@ -3,9 +3,40 @@ import styles from "./EditQuiz.module.css";
 export default function EditQuiz() {
   return (
     <div>
+      <hr className={styles.hrColor}></hr>
       <div className={styles.quizContainer}>
-        <div className={styles.sidebar}>sidebar</div>
-        <div className={styles.main}>main</div>
+        <div className={styles.sidebar}>
+          <div className={styles.qustionContainer}>
+            <p className={styles.questNum}>1</p>
+            <p>Qustion Text</p>
+          </div>
+          <div className={styles.qustionContainer}>
+            <p className={styles.questNum}>1</p>
+            <p>Qustion Text</p>
+          </div>
+        </div>
+        <div className={styles.main}>
+          <div className={styles.questionHeader}>
+            <p>Add Qustion</p>
+            <p> X/X Added</p>
+          </div>
+
+          <form>
+            <input placeholder="title"></input>
+            <div className={styles.correctContainer}>
+              <p>Options</p>
+              <p>Correct</p>
+            </div>
+            <input placeholder="option 1"></input>
+            <input type="checkbox"></input>
+            <input placeholder="option 2"></input>
+            <input type="checkbox"></input>
+            <input placeholder="option 3"></input>
+            <input type="checkbox"></input>
+            <input placeholder="option 4"></input>
+            <input type="checkbox"></input>
+          </form>
+        </div>
       </div>
     </div>
   );
