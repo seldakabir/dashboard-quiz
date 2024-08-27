ecc7b0fedb9ac92934b7da75a8f1ed22214fa07f
selda kabir <seldaakabirnejat@gmail.com>
2024-08-27 12:27:48 +0300

    finishedEditQuiz

--- src/Pages/EditQuiz/EditQuiz.js
+++ src/Pages/EditQuiz/EditQuiz.js
@@ -2,7 +2,7 @@ import React from "react";
 import styles from "./EditQuiz.module.css";
 export default function EditQuiz() {
   return (
-    <div>
+    <div className={styles.container}>
       <hr className={styles.hrColor}></hr>
       <div className={styles.quizContainer}>
         <div className={styles.sidebar}>
@@ -18,8 +18,9 @@ export default function EditQuiz() {
         <div className={styles.main}>
           <div className={styles.questionHeader}>
             <p>Add Qustion</p>
-            <p> X/X Added</p>
+            <p className={styles.hrNum}> X/X Added</p>
           </div>
+          <div className={styles.line}></div>
 
           <form>
             <input placeholder="title"></input>
@@ -27,14 +28,38 @@ export default function EditQuiz() {
               <p>Options</p>
               <p>Correct</p>
             </div>
-            <input placeholder="option 1"></input>
-            <input type="checkbox"></input>
-            <input placeholder="option 2"></input>
-            <input type="checkbox"></input>
-            <input placeholder="option 3"></input>
-            <input type="checkbox"></input>
-            <input placeholder="option 4"></input>
-            <input type="checkbox"></input>
+            <div className={styles.optionContainer}>
+              <input placeholder="option 1" className={styles.options}></input>
+              <input type="radio" value="option1" checked={true} />{" "}
+            </div>
+            <div className={styles.optionContainer}>
+              <input placeholder="option 2" className={styles.options}></input>
+              <input type="radio" value="option1" />{" "}
+            </div>
+            <div className={styles.optionContainer}>
+              <input placeholder="option 3" className={styles.options}></input>
+              <input type="radio" value="option1" />{" "}
+            </div>
+            <div className={styles.optionContainer}>
+              <input placeholder="option 4" className={styles.options}></input>
+              <input type="radio" value="option1" />{" "}
+            </div>
+            <div>
+              <div>
+                *Marking a correct option is only allowed after you have already{" "}
+                written all options.
+              </div>
+              <div className={styles.note}>
+                Note: Please be carefull before creating a question because if
+                you have to later edit it you will lose all responses to the
+                question. This is done so that we can provide you better and
+                correct staticts.
+              </div>
+            </div>
+            <div className={styles.buttons}>
+              <button>Cancel</button>
+              <button>SUBMIT</button>
+            </div>
           </form>
         </div>
       </div>
