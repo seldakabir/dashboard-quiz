a6042d30d6d8d4cf5b499d8302b3123b0e676c9f
selda kabir <seldaakabirnejat@gmail.com>
2024-08-29 00:29:58 +0300

    addStatus

--- src/Contexts/QuizProvider.js
+++ src/Contexts/QuizProvider.js
@@ -3,6 +3,16 @@ const QuizContext = createContext();
 
 function QuizProvider({ children }) {
   const [quizs, setQuizs] = useState([]);
+  const [title, setQuizTitle] = useState("");
+  const [description, setQuizDescription] = useState("");
+  const [totalQuestions, setQuizTotalQuestions] = useState(0);
+  const [status, setQuizStatus] = useState(true);
+  const [topic, setQuizTopic] = useState([]);
+  const [topic1, setQuizTopic1] = useState("");
+  const [topic2, setQuizTopic2] = useState("");
+  const [topic3, setQuizTopic3] = useState("");
+  const [topic4, setQuizTopic4] = useState("");
+
   useEffect(function () {
     async function fetchQuiz() {
       try {
@@ -17,8 +27,58 @@ function QuizProvider({ children }) {
     fetchQuiz();
   }, []);
 
+  function createQuizSubmit(e) {
+    e.preventDefault();
+    if (!description || !title || !totalQuestions)
+      return alert("please complete the information");
+    const newQuiz = {
+      id: quizs.length + 1,
+      title,
+      description,
+      totalQuestions,
+      status,
+      topic: [topic1, topic2, topic3],
+    };
+    addQuiz(newQuiz);
+    setQuizDescription("");
+    setQuizTitle("");
+    setQuizStatus(true);
+    setQuizTotalQuestions(0);
+    setQuizTopic([]);
+  }
+  function addQuiz(quiz) {
+    setQuizs((quizs) => [...quizs, quiz]);
+  }
+  useEffect(() => {
+    console.log("Updated quizs:", quizs);
+  }, [quizs]);
+
   return (
-    <QuizContext.Provider value={{ quizs, setQuizs }}>
+    <QuizContext.Provider
+      value={{
+        quizs,
+        setQuizs,
+        createQuizSubmit,
+        setQuizTitle,
+        setQuizDescription,
+        setQuizTotalQuestions,
+        setQuizStatus,
+        setQuizTopic,
+        title,
+        description,
+        totalQuestions,
+        status,
+        topic,
+        topic1,
+        setQuizTopic1,
+        topic2,
+        setQuizTopic2,
+        topic3,
+        setQuizTopic3,
+        topic4,
+        setQuizTopic4,
+      }}
+    >
       {children}
     </QuizContext.Provider>
   );
--- src/Pages/CreateQuiz/CreateQuiz.js
+++ src/Pages/CreateQuiz/CreateQuiz.js
@@ -1,21 +1,85 @@
 import React from "react";
 import styles from "./CreateQuiz.module.css";
+import { QuizProvider, UseQuiz } from "../../Contexts/QuizProvider";
 export default function CreateQuiz() {
+  const {
+    createQuizSubmit,
+    setQuizTitle,
+    setQuizDescription,
+    setQuizTotalQuestions,
+    setQuizStatus,
+
+    title,
+    description,
+    totalQuestions,
+    status,
+
+    topic1,
+    setQuizTopic1,
+    topic2,
+    setQuizTopic2,
+    topic3,
+    setQuizTopic3,
+    topic4,
+    setQuizTopic4,
+  } = UseQuiz();
   return (
     <div className={styles.container}>
-      <form>
+      <form onSubmit={createQuizSubmit}>
         <h1> Quiz Information</h1>
-        <input type="text" placeholder="Enter Title"></input>
-        <input type="text" placeholder="Enter Explain"></input>
-        <input type="text" placeholder="Enter Title"></input>
-        <input type="number" placeholder="Enter Total Questions"></input>
-        <h2>Enter Topics:</h2>
-        <div className={styles.topics}>
-          <input type=""></input>
-          <input type=""></input>
-          <input type=""></input>
-          <input type=""></input>
+        <input
+          type="text"
+          placeholder="Enter Title"
+          value={title}
+          onChange={(e) => setQuizTitle(e.target.value)}
+        ></input>
+        <input
+          type="text"
+          placeholder="Enter Description"
+          value={description}
+          onChange={(e) => setQuizDescription(e.target.value)}
+        ></input>
+        <div className={styles.select}>
+          <select
+            value={status}
+            onChange={(e) => setQuizStatus(e.target.value)}
+          >
+            <option>Enter status</option>
+            <option value={true}>Active</option>
+            <option value={false}>Deactive</option>
+          </select>
+          <input
+            type="number"
+            placeholder="Enter Total Questions"
+            value={totalQuestions}
+            onChange={(e) => setQuizTotalQuestions(Number(e.target.value))}
+          ></input>
+          <div className={styles.topics}>
+            <h2>Enter Topics:</h2>
+
+            <input
+              type=""
+              value={topic1}
+              onChange={(e) => setQuizTopic1(e.target.value)}
+            ></input>
+            <input
+              type=""
+              value={topic2}
+              onChange={(e) => setQuizTopic2(e.target.value)}
+            ></input>
+            <input
+              type=""
+              value={topic3}
+              onChange={(e) => setQuizTopic3(e.target.value)}
+            ></input>
+            <input
+              type=""
+              value={topic4}
+              onChange={(e) => setQuizTopic4(e.target.value)}
+            ></input>
+          </div>
         </div>
+
         <button>Add Questions</button>
       </form>
     </div>
