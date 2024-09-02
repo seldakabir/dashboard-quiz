3b486031b08f3939671b02ec18fb294c91329cd6
selda kabir <seldaakabirnejat@gmail.com>
2024-09-02 17:01:50 +0300

    startQuiz

--- src/Pages/QuizStartPage/QuizStartPage.js
+++ src/Pages/QuizStartPage/QuizStartPage.js
@@ -1,7 +1,13 @@
 import React from "react";
 import styles from "./QuizStartPage.module.css";
 import { Button } from "react-bootstrap";
+import { UseQuiz } from "../../Contexts/QuizProvider";
+import { useParams } from "react-router-dom";
 export default function QuizStartPage() {
+  const [quizs] = UseQuiz();
+  const { quizId } = useParams();
+  const selectedQuiz = quizs.find((q) => q.id === Number(quizId));
+
   return (
     <div className={styles.container}>
       <div className={styles.sidebar}>
@@ -10,7 +16,7 @@ export default function QuizStartPage() {
       <div className={styles.main}>
         <div className={styles.header}>
           <p>Question X</p>
-          <p>X/X Completed</p>
+          <p>`"X/"${selectedQuiz} Completed`</p>
         </div>
         <form className={styles.page}>
           <div className={styles.question}>QUESTİON</div>
